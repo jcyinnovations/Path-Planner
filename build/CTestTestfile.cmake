2776d9e20d700a6b6665b24a78f120dbe31433fc
Joel Yarde <jcyinnovations@outlook.com>
2018-01-06 21:48:56 -0500

    remove build artifacts

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jyarde/Udacity/CarND-Path-Planning-Project
-# Build directory: /home/jyarde/Udacity/CarND-Path-Planning-Project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TrajectoryTests "/home/jyarde/Udacity/CarND-Path-Planning-Project/build/planner_tests")
-subdirs(src)
-subdirs(tests)
--- build/tests/CMakeFiles/path_planning_test_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libpath_planning_test_lib.a"
-)
